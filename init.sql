72876a388357331f1df3fdb35d2b8993184b482b
ZaYtI <antonin_riquart@ens.univ-artois.fr>
2024-10-03 00:31:27 +0200

    🌱 Add seeder for database

---
+++ init.sql
@@ -0,0 +1,80 @@
+DROP DATABASE IF EXISTS insa;
+
+CREATE DATABASE insa;
+
+USE insa;
+
+DROP TABLE IF EXISTS InscriptionChallenge;
+DROP TABLE IF EXISTS InscriptionActivite;
+DROP TABLE IF EXISTS Challenge;
+DROP TABLE IF EXISTS Activite;
+DROP TABLE IF EXISTS Etudiant;
+DROP TABLE IF EXISTS Equipe;
+DROP TABLE IF EXISTS Formation;
+
+CREATE TABLE formation (
+    id_formation SERIAL PRIMARY KEY,
+    diplome VARCHAR(255),
+    annee INT,
+    departement VARCHAR(255)
+);
+
+CREATE TABLE equipe (
+    id_equipe SERIAL PRIMARY KEY,
+    nom VARCHAR(255),
+    slogan VARCHAR(255),
+    nb_points INT DEFAULT 0,
+    CONSTRAINT UC_Nom UNIQUE (Nom)
+);
+
+CREATE TABLE etudiant (
+    id_etudiant SERIAL PRIMARY KEY,
+    nom VARCHAR(255),
+    prenom VARCHAR(255),
+    adresse VARCHAR(255),
+    nb_points INT DEFAULT 0,
+    id_formation INT,
+    id_equipe INT,
+    FOREIGN KEY (id_formation) REFERENCES formation(id_formation) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_equipe) REFERENCES equipe(id_equipe) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+CREATE TABLE activite (
+    id_activite SERIAL PRIMARY KEY,
+    nom VARCHAR(255),
+    date_activite DATE,
+    lieu VARCHAR(255),
+    duree INT,
+    descriptif TEXT,
+    nb_points INT,
+    nb_max INT,
+    CONSTRAINT UC_NomActivite UNIQUE (Nom)
+);
+
+CREATE TABLE inscription_activite (
+    id_activite INT,
+    id_etudiant INT,
+    PRIMARY KEY (id_activite, id_etudiant),
+    FOREIGN KEY (id_activite) REFERENCES activite(id_activite) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_etudiant) REFERENCES etudiant(id_etudiant) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE challenge (
+    id_challenge SERIAL PRIMARY KEY,
+    nom VARCHAR(255),
+    date_challenge DATE,
+    lieu VARCHAR(255),
+    duree INT,
+    descriptif TEXT,
+    nb_points INT,
+    nb_equipes INT,
+    CONSTRAINT UC_NomChallenge UNIQUE (Nom)
+);
+
+CREATE TABLE inscription_challenge (
+    id_challenge INT,
+    id_equipe INT,
+    PRIMARY KEY (id_challenge, id_equipe),
+    FOREIGN KEY (id_challenge) REFERENCES challenge(id_challenge) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_equipe) REFERENCES equipe(id_equipe) ON DELETE CASCADE ON UPDATE CASCADE
+);
---
+++ seed.py
@@ -0,0 +1,255 @@
+import psycopg2
+from faker import Faker
+import random
+import getpass
+import os
+from dotenv import load_dotenv
+
+class DatabaseConnection:
+    def __init__(self, database, user, server, password, port):
+        self.database = database
+        self.user = user
+        self.server = server
+        self.password = password
+        self.port = port
+        self.conn = None
+
+    def connect(self):
+        if self.conn is None:
+            self.conn = psycopg2.connect(
+                database=self.database,
+                user=self.user,
+                server=self.server,
+                password=self.password,
+                port=self.port
+            )
+        return self.conn
+
+    def close(self):
+        if self.conn is not None:
+            self.conn.close()
+            self.conn = None
+
+class BaseTable:
+    def __init__(self, db_conn):
+        self.conn = db_conn
+        self.cur = self.conn.cursor()
+        self.fake = Faker()
+
+    def create(self, table_name, attributes):
+        columns = ', '.join(attributes.keys())
+        placeholders = ', '.join(['%s'] * len(attributes))
+        query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders});"
+        self.cur.execute(query, list(attributes.values()))
+        self.conn.commit()
+
+    def delete_all(self, table_name):
+        query = f"DELETE FROM {table_name};"
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def close(self):
+        self.cur.close()
+
+    def get_list_of_id_in_table(self, table_name):
+        query = f"SELECT id FROM {table_name};"
+        self.cur.execute(query)
+        ids = self.cur.fetchall()
+        return [row[0] for row in ids]
+
+class Activite(BaseTable):
+    def __init__(self, db_conn):
+        super().__init__(db_conn)
+        self.table_name = "Activite"
+        self.attributes = {
+            "Nom": random.choice(['Basket','Foot','Badminton','Handball','VTT','Cyclo','Bazoom']),
+            "date_activite": self.fake.date(),
+            "lieu": self.fake.city(),
+            "duree": random.randint(1,120),
+            "descriptif": self.fake.text(),
+            "nb_points": random.randint(0,1000),
+            "nb_max": random.randint(0,1000)
+        }
+
+        print(self.attributes)
+
+        self.create(self.table_name, self.attributes)
+
+class Equipe(BaseTable):
+    def __init__(self, db_conn):
+        super().__init__(db_conn)
+        self.table_name = "Equipe"
+        self.attributes = {
+            "nom": self.fake.name(),
+            "slogan": self.fake.catch_phrase(),
+            "nb_points": random.randint(0,1000)
+        }
+        print(self.attributes)
+        self.create(self.table_name, self.attributes)
+
+class Etudiant(BaseTable):
+    def __init__(self, db_conn):
+        super().__init__(db_conn)
+        self.table_name = "Etudiant"
+        self.attributes = {
+            "nom":self.fake.last_name(),
+            "prenom":self.fake.first_name(),
+            "adresse":self.fake.address(),
+            "nb_points": random.randint(0,1000),
+            "id_formation": random.choice(self.get_list_of_id_in_table('Formation')),
+            "id_equipe": random.choice(self.get_list_of_id_in_table('Equipe'))
+        }
+        print(self.attributes)
+        self.create(self.table_name, self.attributes)
+
+class Formation(BaseTable):
+    def __init__(self,db_conn):
+        super().__init__(db_conn)
+        self.table_name = "Formation"
+        self.attributes = {
+            "diplome" : random.choice(['Licence', 'Master', 'Doctorat', 'Diplôme', 'Certificat']),
+            "annee" : self.fake.year(),
+            "departement" : self.fake.department_name()
+        }
+        print(self.attributes)
+        self.create(self.table_name,self.attributes)
+
+class Challenge(BaseTable):
+    
+    def __init__(self,db_conn):
+        super.__init__(db_conn)
+        self.table_name = "Challenge"
+        self.attributes = {
+            "nom": self.fake.word(),
+            "date_challenge": self.fake.date(),
+            "lieu":self.fake.country(),
+            "duree":random.randint(1,120),
+            "descriptif":self.fake.text()
+        }
+        print(self.attributes)
+        self.create(self.table_name,self.attributes)
+
+class InscriptionActivite(BaseTable):
+
+    def __init__(self, db_conn):
+        super().__init__(db_conn)
+        self.table_name = "InscriptionActivite"
+        self.attributes = {
+            "id_activite": random.choice(self.get_list_of_id_in_table('Activite')),
+            "id_etudiant":random.choice(self.get_list_of_id_in_table('Etudiant')),
+        }
+        print(self.attributes)
+        self.create(self.table_name,self.attributes)
+
+class InscriptionChallenge(BaseTable):
+
+    def __init__(self, db_conn):
+        super().__init__(db_conn)
+        self.table_name = "InscriptionChallenge"
+        self.attributes = {
+            "id_challenge": random.choice(self.get_list_of_id_in_table('Challenge')),
+            "id_equipe":random.choice(self.get_list_of_id_in_table('Equipe')),
+        }
+        self.create(self.table_name,self.attributes)
+
+def main():
+    load_dotenv()
+
+    DATABASE, USER, SERVER, PASSWORD, PORT = get_database_credentials()
+    SEPARATOR = '/////////////////////////////////////////////////////'
+    
+    db_conn = DatabaseConnection(DATABASE, USER, SERVER, PASSWORD, PORT)
+    
+    display_initial_menu(SEPARATOR)
+
+    choice = int(input("Entrer votre choix :"))
+    
+    if choice == 1:
+        row_number = int(input("Combien de tuples voulez-vous insérer dans chaque table : "))
+        insert_in_all_table(row_number, db_conn)
+    else:
+        table_classes = get_table_classes()
+        handle_table_selection(table_classes, db_conn)
+
+import os
+import getpass
+
+def get_database_credentials():
+    DATABASE = os.getenv('DATABASE') or input("Entrer le nom de votre base de donnée : ")
+    USER = os.getenv('USER') or input('Entrer le nom utilisateur : ')
+    SERVER = os.getenv('SERVER') or input('Entrer le serveur : ')
+    PASSWORD = os.getenv('PASSWORD') or getpass.getpass('Entrer le mot de passe : ')
+    PORT = int(os.getenv('PORT')) or int(input('Entrer le port : '))
+    return DATABASE, USER, SERVER, PASSWORD, PORT
+
+def display_initial_menu(separator):
+    print('\n')
+    print(separator)
+    print(separator)
+    print('\n')
+    print('1. Ajouter des données dans toutes les tables')
+    print('2. Ajouter des données en sélectionnant les tables')
+    print('\n')
+    print(separator)
+    print(separator)
+    print('\n')
+
+def get_table_classes():
+    return {
+        1: Activite,
+        2: Equipe,
+        3: Etudiant,
+        4: Formation,
+        5: Challenge,
+        6: InscriptionActivite,
+        7: InscriptionChallenge
+    }
+
+def handle_table_selection(table_classes, db_conn):
+    while True:
+        display_table_selection_menu()
+        table_choice = int(input("Entrer le numéro de la table : "))
+
+        if table_choice == 8:
+            break
+
+        row_number = int(input("Entrer le nombre de tuples voulus : "))
+
+        if row_number != 0 and table_choice in table_classes:
+            for _ in range(row_number):
+                table_classes[table_choice](db_conn)
+
+def display_table_selection_menu():
+    SEPARATOR = '/////////////////////////////////////////////////////'
+    print('\n')
+    print(SEPARATOR)
+    print(SEPARATOR)
+    print('\n')
+    print("Choisissez les tables dans lesquelles vous voulez ajouter des données : ")
+    print("1. Activite")
+    print("2. Equipe")
+    print("3. Etudiant")
+    print("4. Formation")
+    print("5. Challenge")
+    print("6. InscriptionActivite")
+    print("7. InscriptionChallenge")
+    print("8. Quitter")
+    print('\n')
+    print(SEPARATOR)
+    print(SEPARATOR)
+    print('\n')
+
+def insert_in_all_table(row_number,db_conn):
+    for _ in range(row_number):
+        Formation(db_conn)
+        Equipe(db_conn)
+        Activite(db_conn)
+        Challenge(db_conn)
+    
+    for _ in range(row_number):
+        Etudiant(db_conn)
+        InscriptionActivite(db_conn)
+        InscriptionChallenge(db_conn)
+
+if __name__ == "__main__":
+    main()
